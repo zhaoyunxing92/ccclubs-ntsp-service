876fba7e6027299db9f59eb83cf375b8c70040b5
zhaoyunxing92 <2385585770@qq.com>
2018-05-03 17:31:55 +0800

    添加：支付接口

---
+++ src/main/java/com/ccclubs/ntsp/server/service/NewTspPayService.java
@@ -0,0 +1,10 @@
+package com.ccclubs.ntsp.server.service;
+
+/**
+ * @author sunny
+ * @class: com.ccclubs.ntsp.server.service.NewTspPayService
+ * @date: 2018-05-03 17:30
+ * @des: 支付模块
+ */
+public interface NewTspPayService {
+}
