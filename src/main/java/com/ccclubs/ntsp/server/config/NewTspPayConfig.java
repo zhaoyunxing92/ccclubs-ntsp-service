026e9924166f844dc4a9fbdc9983acf5f443d864
zhaoyunxing <2385585770@qq.com>
2018-05-03 18:26:57 +0800

    添加：支付配置class

---
+++ src/main/java/com/ccclubs/ntsp/server/config/NewTspPayConfig.java
@@ -0,0 +1,10 @@
+package com.ccclubs.ntsp.server.config;
+
+/**
+ * @author sunny
+ * @class: com.ccclubs.ntsp.server.config.NewTspPayConfig
+ * @date: 2018-05-03 18:25
+ * @des:
+ */
+public class NewTspPayConfig {
+}
