879f5aa5ae457b762d2d8cb0eb8d34f453f384f0
zhaoyunxing <2385585770@qq.com>
2018-05-05 15:52:19 +0800

    添加：日志

--- src/main/java/com/ccclubs/ntsp/server/controller/example/HelloController.java
+++ src/main/java/com/ccclubs/ntsp/server/controller/example/HelloController.java
@@ -1,6 +1,8 @@
 package com.ccclubs.ntsp.server.controller.example;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @GetMapping("/say")
   public String say(@NotEmpty(message = "name不为空") String name) {
+
     return "hello " + name;
   }
 }
